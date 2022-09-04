0c60198a2e730536c2b257112cb8b344787aa25b
seanluse41 <seanluse41@gmail.com>
2022-09-05 00:25:12 +0900

    add fukutoshin

---
+++ src/constants/fukutoshin.js
@@ -0,0 +1,219 @@
+let fukutoshinLine = [
+    {
+        name: 'Wakoshi',
+        id: 1,
+        type: 'metro',
+        line: 'fukutoshin',
+        transfer: false,
+        transferInfo: []
+    },
+    {
+        name: 'Chikatetsu-narimasu',
+        id: 2,
+        type: 'metro',
+        line: 'fukutoshin',
+        transfer: false,
+        transferInfo: []
+    },
+    {
+        name: 'Chikatetsu-akatsuka',
+        id: 3,
+        type: 'metro',
+        line: 'fukutoshin',
+        transfer: false,
+        transferInfo: []
+    },
+    {
+        name: 'Heiwadai',
+        id: 4,
+        type: 'metro',
+        line: 'fukutoshin',
+        transfer: false,
+        transferInfo: []
+    },
+    {
+        name: 'Hikawadai',
+        id: 5,
+        type: 'metro',
+        line: 'fukutoshin',
+        transfer: false,
+        transferInfo: []
+    },
+    {
+        name: 'Kotake-mukaihara',
+        id: 6,
+        type: 'metro',
+        line: 'fukutoshin',
+        transfer: false,
+        transferInfo: []
+    },
+    {
+        name: 'Senkawa',
+        id: 7,
+        type: 'metro',
+        line: 'fukutoshin',
+        transfer: false,
+        transferInfo: []
+    },
+    {
+        name: 'Kanamecho',
+        id: 8,
+        type: 'metro',
+        line: 'fukutoshin',
+        transfer: false,
+        transferInfo: []
+    },
+    {
+        name: 'Ikebukuro',
+        id: 9,
+        type: 'metro',
+        line: 'fukutoshin',
+        transfer: true,
+        transferInfo: [
+            {
+                type: 'jr',
+                line: 'yamanote',
+                name: 'Ikebukuro',
+                id: 0
+            },
+            {
+                type: 'jr',
+                line: 'saikyo',
+                name: 'Ikebukuro',
+                id: 0
+            },
+            {
+                type: 'metro',
+                line: 'marunouchi',
+                name: 'Ikebukuro',
+                id: 25
+            },
+            {
+                type: 'metro',
+                line: 'yurakucho',
+                name: 'Ikebukuro',
+                id: 9
+            },
+        ]
+    },
+    {
+        name: 'Zoshigaya',
+        id: 10,
+        type: 'metro',
+        line: 'fukutoshin',
+        transfer: false,
+        transferInfo: []
+    },
+    {
+        name: 'Nishi-waseda',
+        id: 11,
+        type: 'metro',
+        line: 'fukutoshin',
+        transfer: false,
+        transferInfo: []
+    },
+    {
+        name: 'Higashi-shinjuku',
+        id: 12,
+        type: 'metro',
+        line: 'fukutoshin',
+        transfer: true,
+        transferInfo: [
+            {
+                type: 'metro',
+                line: 'oedo',
+                name: 'Higashi-shinjuku',
+                id: 2
+            }
+        ]
+    },
+    {
+        name: 'Shinjuku-sanchome',
+        id: 13,
+        type: 'metro',
+        line: 'fukutoshin',
+        transfer: true,
+        transferInfo: [
+            {
+                type: 'metro',
+                line: 'marunouchi',
+                name: 'Shinjuku-sanchome',
+                id: 9
+            },
+            {
+                type: 'metro',
+                line: 'shinjuku',
+                name: 'Shinjuku-sanchome',
+                id: 2
+            }
+        ]
+    },
+    {
+        name: 'Kita-sando',
+        id: 14,
+        type: 'metro',
+        line: 'fukutoshin',
+        transfer: false,
+        transferInfo: []
+    },
+    {
+        name: 'Meiji-jingumae',
+        id: 15,
+        type: 'metro',
+        line: 'fukutoshin',
+        transfer: true,
+        transferInfo: [
+            {
+                type: 'metro',
+                line: 'chiyoda',
+                name: 'Meiji-jingumae',
+                id: 3
+            },
+            {
+                type: 'jr',
+                line: 'yamanote',
+                name: 'Harajuku',
+                id: 0
+            }
+        ]
+    },
+    {
+        name: 'Shibuya',
+        id: 16,
+        type: 'metro',
+        line: 'fukutoshin',
+        transfer: true,
+        transferInfo: [
+            {
+                type: 'jr',
+                line: 'yamanote',
+                name: 'Shibuya',
+                id: 0
+            },
+            {
+                type: 'metro',
+                line: 'hanzomon',
+                name: 'Shibuya',
+                id: 1
+            },
+            {
+                type: 'metro',
+                line: 'ginza',
+                name: 'Shibuya',
+                id: 1
+            },
+            {
+                type: 'jr',
+                line: 'saikyo',
+                name: 'Shibuya',
+                id: 0
+            },
+            {
+                type: 'jr',
+                line: 'den-en-toshi',
+                name: 'Shibuya',
+                id: 0
+            },
+        ]
+    },
+];
